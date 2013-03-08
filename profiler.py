e5707b796047b0318f3a134eaf13b3ce724a9f9b
Attila-Mihaly Balazs <dify.ltd@gmail.com>
2013-03-08 16:52:56 +0200

    Log a warning in case RequestStats is too big to store in Memcache

--- profiler.py
+++ profiler.py
@@ -269,7 +269,10 @@ class RequestStats(object):
         pickled = pickle.dumps(self)
         compressed_pickled = zlib.compress(pickled)
         if len(compressed_pickled) > memcache.MAX_VALUE_SIZE:
-            return False
+            logging.warning('RequestStats bigger (%d) '
+	        + 'than max memcache size (%d), even after compression',
+		len(compressed_pickled), memcache.MAX_VALUE_SIZE)
+	    return False
 
         return memcache.set(RequestStats.memcache_key(self.request_id), compressed_pickled)
 
